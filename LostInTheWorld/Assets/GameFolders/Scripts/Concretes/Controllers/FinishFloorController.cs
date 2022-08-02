0f1dc16020df4897cfbbdeb2d19a749d06225cbb
emiruyan <emir-uyn@hotmail.com>
2022-08-02 18:41:30 +0300

    Finish Floor
    Added finish floor particle effect.

--- LostInTheWorld/Assets/GameFolders/Scripts/Concretes/Controllers/FinishFloorController.cs
+++ LostInTheWorld/Assets/GameFolders/Scripts/Concretes/Controllers/FinishFloorController.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FinishFloorController : MonoBehaviour
+namespace LostInTheWorld.Controllers
 {
-    // Start is called before the first frame update
-    void Start()
+    public class FinishFloorController : MonoBehaviour
     {
-        
+      
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
