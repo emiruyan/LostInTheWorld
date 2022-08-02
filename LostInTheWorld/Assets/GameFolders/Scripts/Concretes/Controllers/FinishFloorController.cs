a712bcc901a6cc0f8a689742178fd881dd29b33d
emiruyan <emir-uyn@hotmail.com>
2022-08-02 19:00:32 +0300

    Finish Floor Scripts
    Added Finish Floor Scripts. Now the particle effect is activated when the player touches the finish floor.

--- LostInTheWorld/Assets/GameFolders/Scripts/Concretes/Controllers/FinishFloorController.cs
+++ LostInTheWorld/Assets/GameFolders/Scripts/Concretes/Controllers/FinishFloorController.cs
@@ -1,12 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace LostInTheWorld.Controllers
 {
     public class FinishFloorController : MonoBehaviour
     {
-      
+        [SerializeField] GameObject _finishParticleEffect;//Particle effecti verdil.
+
+        private void OnCollisionEnter(Collision other) //İki nesne arasında çarpışma gerçekleştiğinde
+        {
+            PlayerController player = other.collider.GetComponent<PlayerController>();//Player ve collider çarpıştığında 
+
+            if (player == null) //player null mı ?
+            {
+                return; //nullsa return et
+            }
+
+            if (other.GetContact(0).normal.y == -1)//Player collider'a tam tepeden y pozisyonundan mı değiyor ?
+            {
+                _finishParticleEffect.gameObject.SetActive(true);//Particle effecti' devreye sok
+            }
+            else
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //Oynanan sahneyi tekrar yükle(Game Over)
+            }
+        }
     }
 
 }
