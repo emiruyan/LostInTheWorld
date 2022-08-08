3d621d18558725f040b48847f583439389dd878e
emiruyan <emir-uyn@hotmail.com>
2022-08-08 17:56:11 +0300

    Game Over Panel
    Game Over Panel operations are completed. And the script is complete. GameOverPanel becomes active when we die.

---
+++ LostInTheWorld/Assets/GameFolders/Scripts/Concretes/Uis/GameOverObject.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using LostInTheWorld.Managers;
+using UnityEngine;
+
+namespace LostInTheWorld.Uis
+{
+    public class GameOverObject : MonoBehaviour
+    {
+        [SerializeField] GameObject _gameOverPanel;
+
+        private void Awake()
+        {
+            if (_gameOverPanel.activeSelf) //Game Over Panel Açık(True) ise bunu 
+            {
+                _gameOverPanel.SetActive(false); //False'a çeksin
+            }
+        }
+
+        private void OnEnable()
+        {
+            GameManager.Instance.OnGameOver += HandleOnGameOver;
+        }
+        
+        private void OnDisable()
+        {
+            GameManager.Instance.OnGameOver -= HandleOnGameOver;
+        }
+         
+        private void HandleOnGameOver()
+        {
+            if (!_gameOverPanel.activeSelf) //Game Over Panel Kapalı(False) ise
+            {
+                _gameOverPanel.SetActive(true); //True'a çeksin
+            }
+        }
+    }
+}
+
+
